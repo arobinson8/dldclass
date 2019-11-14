b33c7391d5c7b96802e3fd41a597dae7f7fc085a
arobinson8 <57469039+arobinson8@users.noreply.github.com>
2019-11-13 17:25:55 -0800

    updated from 8bitworkshop.com

--- test_hvsync.v
+++ test_hvsync.v
@@ -4,6 +4,14 @@ module and_gate( a, b, y);
   input a, b; // input ports
   output y;  // output ports
   
-  assign y = a & b ; // (a and b )
+  assign y = a | b ; 
   
 endmodule // no semicolon
+
+
+module and_gate_top(
+output y
+);
+  and_gate  uut(0, 0, y) ;
+  
+endmodule
